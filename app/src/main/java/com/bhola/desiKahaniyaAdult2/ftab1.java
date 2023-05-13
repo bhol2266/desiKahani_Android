e4a04ad4af9036855a6bc036b5b043eecd5c3bd4
baldgenie <siddhartha.kapoor@gmail.com>
2023-05-14 02:31:28 +0545

    commit

--- app/src/main/java/com/bhola/desiKahaniyaAdult2/ftab1.java
+++ app/src/main/java/com/bhola/desiKahaniyaAdult2/ftab1.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.GridLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -45,26 +46,26 @@ public class ftab1 extends Fragment {
             //fake content while upadting app
             String[] Category_List = {"प्रेम कहानी 1", "प्रेम कहानी 2", "प्रेम कहानी 3", "प्रेम कहानी 4", "प्रेम कहानी 5", "प्रेम कहानी 6"};
             createGridItems(Category_List, view);
-            SplashScreen.DB_TABLE_NAME="FakeStory";
+            SplashScreen.DB_TABLE_NAME = "FakeStory";
         } else {
 
             if (SplashScreen.Login_Times < 4) {
                 //Mixed content
                 String[] Category_List = {"प्रेम कहानी 1", "प्रेम कहानी 2", "प्रेम कहानी 3", "देसी कहानी 1", "देसी कहानी 2", "प्रेम कहानी 6"};
                 createGridItems(Category_List, view);
-                SplashScreen.DB_TABLE_NAME="FakeStory";
+                SplashScreen.DB_TABLE_NAME = "FakeStory";
 
             } else if (SplashScreen.Login_Times < 6) {
                 // censored Content
                 String[] Category_List = {"देसी कहानी 1", "देसी कहानी 2", "देसी कहानी 3", "देसी कहानी 4", "देसी कहानी 5", "देसी कहानी 6"};
                 createGridItems(Category_List, view);
-                SplashScreen.DB_TABLE_NAME="FakeStory";
+                SplashScreen.DB_TABLE_NAME = "FakeStory";
 
             } else {
                 // full Content
                 String[] Category_List = {"Latest Stories", "Aunty Sex Story", "Bhabhi Sex", "Desi Kahani", "Family Sex Stories", "First Time Sex", "Gay Sex Stories", "Group Sex Stories", "Indian Sex Stories", "Sali Sex", "Teacher Sex", "Teenage Girl", "XXX Kahani", "अन्तर्वासना", "हिंदी सेक्स स्टोरीज"};
                 createGridItems(Category_List, view);
-                SplashScreen.DB_TABLE_NAME="StoryItems";
+                SplashScreen.DB_TABLE_NAME = "StoryItems";
 
             }
         }
@@ -80,18 +81,65 @@ public class ftab1 extends Fragment {
 
             String category = Category_List[i].replace("Sex", "").replace("सेक्स", "").replace("XXX", "");
 
-
             View vieww = getLayoutInflater().inflate(R.layout.homepage_griditem, null);
             TextView categoryTextView = vieww.findViewById(R.id.Textview1);
+            ImageView auntyImageView = vieww.findViewById(R.id.auntyImageView);
             NeumorphCardView cardView = vieww.findViewById(R.id.cardview);
 
             categoryTextView.setText(category);
 
+            switch (i) {
+                case 0:
+                    auntyImageView.setImageResource(R.drawable.aunty1);
+                    break;
+                case 1:
+                    auntyImageView.setImageResource(R.drawable.aunty2);
+                    break;
+                case 2:
+                    auntyImageView.setImageResource(R.drawable.aunty3);
+                    break;
+                case 3:
+                    auntyImageView.setImageResource(R.drawable.aunty4);
+                    break;
+                case 4:
+                    auntyImageView.setImageResource(R.drawable.aunty5);
+                    break;
+                case 5:
+                    auntyImageView.setImageResource(R.drawable.aunty6);
+                    break;
+                case 6:
+                    auntyImageView.setImageResource(R.drawable.aunty7);
+                    break;
+                case 7:
+                    auntyImageView.setImageResource(R.drawable.aunty8);
+                    break;
+                case 8:
+                    auntyImageView.setImageResource(R.drawable.aunty9);
+                    break;
+                case 9:
+                    auntyImageView.setImageResource(R.drawable.aunty10);
+                    break;
+                case 10:
+                    auntyImageView.setImageResource(R.drawable.aunty11);
+                    break;
+                case 11:
+                    auntyImageView.setImageResource(R.drawable.aunty12);
+                    break;
+                case 12:
+                    auntyImageView.setImageResource(R.drawable.aunty13);
+                    break;
+                case 13:
+                    auntyImageView.setImageResource(R.drawable.aunty14);
+                    break;
+                case 14:
+                    auntyImageView.setImageResource(R.drawable.aunty15);
+                    break;
+            }
             DisplayMetrics displayMetrics = new DisplayMetrics();
             getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
             float requiredWidth = (float) (displayMetrics.widthPixels / 2.2);
 
-            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams((int) requiredWidth, 250);
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams((int) requiredWidth, 600);
             cardView.setLayoutParams(params);
 
             int finalI = i;
